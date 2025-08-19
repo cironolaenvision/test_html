8b36d1c2d49cdb73e73f34a68d7db27e1dca4680
Ciro Nola <ciro.nola@envisiontecnologia.com.br>
2025-08-19 16:02:13 -0300

    files

--- test_browser_lib.py
+++ test_browser_lib.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import asyncio
-from test_html_lib.html_tester import html_tester_singleton as tester
+from test_html_lib.html_tester import html_tester_singleton
 import sys
 
 
 async def main():
-    status, errors = await tester.test(html_snippet)
+    status, errors = await html_tester_singleton.test(html_snippet)
     
     print(status)
     print("Errors:")
@@ -17,9 +17,20 @@ def get_file_content(file_name: str):
         return file.read()
     
 command_line_args = sys.argv
-file_name = command_line_args[1]
+if(len(command_line_args) > 1):
+    file_name = command_line_args[1]
+else:
+    file_name = "html_snippet.html"
+    
+if(len(command_line_args) > 2):
+    max_wait_time_seconds = int(command_line_args[2])
+else:
+    max_wait_time_seconds = 2
+    
+print(f"using file: {file_name}, to test with max wait time of {max_wait_time_seconds} seconds")
 
-
+    
+html_tester_singleton.max_wait_time_seconds = max_wait_time_seconds
 html_snippet = get_file_content(file_name)
 
 
